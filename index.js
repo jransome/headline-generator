990a817018728ef8a3f751c1371f901dab2cecf4
James Ransome <jamesransome@msn.com>
2022-03-14 13:07:05 +0000

    automate

---
+++ index.js
@@ -0,0 +1,24 @@
+const input = document.querySelector('#input')
+const generateButton = document.querySelector('#generateBtn')
+const output = document.querySelector('#output')
+
+function shuffle(arr) {
+  const copy = arr.slice()
+  const result = []
+  while (copy.length > 0) {
+    const randomIndex = Math.floor(Math.random() * copy.length)
+    result.push(copy[randomIndex])
+    copy.splice(randomIndex, 1)
+  }
+  return result
+}
+
+generateButton.addEventListener('click', () => {
+  const words = input.value.split(',')
+
+  for (let i = 0; i < 10; i++) {
+    const suggestion = document.createElement('li')
+    suggestion.innerText = shuffle(words).join(' ')
+    output.appendChild(suggestion)
+  }
+})
